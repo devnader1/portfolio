a1ed166a278d2466f935eade7bfdef567ea1db7d
devnader1 <devnader7@gmail.com>
2025-07-11 08:21:50 +0300

    Add files via upload

---
+++ Js/main.app.js
@@ -0,0 +1,8 @@
+function toggleTheme() {
+        document.documentElement.classList.toggle("dark");
+      }
+
+      // Mobile menu toggle
+      function toggleMobileMenu() {
+        document.getElementById("mobile-menu").classList.toggle("hidden");
+      }
